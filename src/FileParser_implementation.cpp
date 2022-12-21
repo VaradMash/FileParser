9659e6f2947731902b72081e96ef07dbb55ff5b9
VaradMash <varadmash2201@gmail.com>
2022-12-21 11:13:03 +0530

    Added code for word count, line count, get line and parsing methods.

--- src/FileParser_implementation.cpp
+++ src/FileParser_implementation.cpp
@@ -15,7 +15,17 @@ FileParser :: FileParser() {
     this->delimiter = ' ';
 }
 
-//  Parameterized constructor implementation
+//  Parameterized constructors implementation
+FileParser :: FileParser(string filename) {
+    /*
+     *  Input   :   Filename
+     *  Utility :   Parameterized constructor
+     *  Output  :   None       
+     */
+    this->filename = filename;
+    this->delimiter = ' ';
+}
+
 FileParser :: FileParser(string filename, char delimiter) {
     /*
      *  Input   :   Filename, delimiter
@@ -24,6 +34,27 @@ FileParser :: FileParser(string filename, char delimiter) {
      */
     this->filename = filename;
     this->delimiter = delimiter;
+}
+
+//  Parser method (PRIVATE)
+void FileParser::parse(string line) {
+    /*
+     *  Input   :   Line    
+     *  Utility :   Split line based on delimiter
+     *  Output  :   Store split to vector
+     */    
+    // Clear previous output
+    this->delimited_line.clear();
+    string data = "";
+    for(char c : line) {
+        if(c == this->delimiter) {
+            this->delimited_line.push_back(data);
+            data = "";
+        } else {
+            data.push_back(c);
+        }
+    }
+    this->delimited_line.push_back(data);    
 }
 
 //  Filename setter
@@ -98,4 +129,39 @@ void FileParser::clear() {
      *  Output  :   None
      */
     this->data_lines.clear();
+}
+
+//  Count words in file
+int FileParser::get_word_count() {
+    /*
+     *  Input   :   Lines, delimiter
+     *  Utility :   Get count of words by parsing each line
+     *  Output  :   integer
+     */
+    int count = 0;
+    for(string line : this->data_lines) {
+        parse(line);
+        count += this->delimited_line.size();        
+    }
+    return count;
+}
+
+int FileParser::get_line_count() {
+    /*
+     *  Input   :   None
+     *  Utility :   Count number of lines in file
+     *  Output  :   integer
+     */
+    return this->data_lines.size();
+}
+
+string FileParser::get_line(int line_number) {
+    /*
+     *  Input   :   Line number (1 indexed)
+     *  Utility :   Get specific line from file
+     *  Output  :   string
+     */
+    if(line_number < 1) return "Invalid line";
+    if(line_number > this->data_lines.size()) return "Line number exceeds file size";
+    return this->data_lines[line_number - 1];
 }
