313c5f664bf990a8186ac7309c19c7548f9ad2e0
VaradMash <varadmash2201@gmail.com>
2022-12-21 11:12:22 +0530

    Added new method signatures.

--- src/FileParser.h
+++ src/FileParser.h
@@ -9,12 +9,14 @@ private:
     string filename;
     char delimiter;
     vector<string> data_lines;
-    void parse();
+    vector<string> delimited_line;  //  Placeholder for parse function result
+    void parse(string);    
     ifstream infile;
     ofstream outfile;
 
 public:
     FileParser();                   //  Default constructor
+    FileParser(string);             //  Parameterized constructor for filename
     FileParser(string, char);       //  Parameterized constructor for filename and delimiter
     void set_filename(string);      //  Setter for filename
     string get_filename();          //  Getter for filename               
@@ -22,5 +24,8 @@ public:
     void clear();                   //  Clear data
     void set_delimiter(char);       //  Setter for filename
     char get_delimiter();           //  Getter for delimiter
-    void display();                 //  Method to display file contents line by line                    
+    void display();                 //  Method to display file contents line by line                 
+    int get_word_count();           //  Method to get word count based on delimiter  
+    int get_line_count();           //  Method to get line count
+    string get_line(int);           //  Method to get specific line from file
 };
